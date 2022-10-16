e6ae2d11736de66e38782324f9121ce76cbbc132
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2022-10-16 12:04:52 -0300

    Add files via upload

--- tcc_parteNicolas-HTML/formulario.js
+++ tcc_parteNicolas-HTML/formulario.js
@@ -18,3 +18,26 @@ input.addEventListener('keypress', () => {
         input.value += "-"
     }
 })
+
+// Animação fundo//
+const ulFundo = document.querySelector("ul.fundo");
+
+for(let e = 0; e < 11; e++){
+
+    const li = document.createElement("li");
+    const random =  (min, max) => Math.random() * (max - min) + min;
+      const size = Math.floor(random(10, 200));
+      const position = random(1, 99);
+        const delay = random(5, 0.1);  
+        const duration = random(24, 12);
+
+      li.style.width = `${size}px`;
+        li.style.height = `${size}px`;
+          li.style.bottom = `-${size}px`;
+            li.style.left = `${position}%`;
+              li.style.animationDelay = `${delay}s`
+              li.style.animationDuration = `${duration}s`
+              li.style,AnimationTimingFunction = `cubic-bazier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`    
+
+      ulFundo.appendChild(li);
+}
