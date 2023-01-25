89e0045daf35acb3970d571e0db32e21895f493c
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2023-01-25 07:24:22 -0300

    Add files via upload

---
+++ tcc-parteNicolas/tcc-master/excluirImagem.php
@@ -0,0 +1,9 @@
+<?php
+$nomeImagem = $_GET['fileName'];
+if (file_exists($nomeImagem)) {
+    unlink($nomeImagem);
+    header("Location: exibeImagens.php");
+}else{
+    echo "<script>alert('erro');</script>";
+}
+?>
