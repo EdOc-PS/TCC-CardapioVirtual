27fdd069c0a5e220b0dec2256db2f00b26827166
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2023-02-04 11:46:05 -0300

    Delete tcc-master directory

--- tcc-master/cancelarPedido.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("location: verPedidos.php");
-include('conexaoPedido.php');
-echo $_POST['idHidden'];
-$deletarPedido = 'DELETE from pedido where id_pedido='.$_POST['idHidden'];
-$conn->query($deletarPedido);
-?>
