0fede8c8f5221d712cf745809b7ad501c256a5a8
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2023-02-04 11:46:28 -0300

    Delete tcc-parteNicolas directory

--- tcc-parteNicolas/tcc-master/excluirImagem.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nomeImagem = $_GET['fileName'];
-if (file_exists($nomeImagem)) {
-    unlink($nomeImagem);
-    header("Location: exibeImagens.php");
-}else{
-    echo "<script>alert('erro');</script>";
-}
-?>
